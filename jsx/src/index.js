2833481159433c7dcc692e91134e6771ba9ae5b0
César Espirandelli <espirandelli@gmail.com>
2023-09-02 15:22:27 -0300

    Declare variable inside return

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -10,10 +10,7 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App(){
-    const date = new Date();
-    const time = date.toLocaleTimeString();
-
-    return <h1>{time}</h1>;
+    return <h1>{new Date().toLocaleTimeString()}</h1>;
 }
 
 // 5) Show the content on the screen
