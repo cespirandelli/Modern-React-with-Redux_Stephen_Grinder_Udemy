6bbf6b1ad19b3b947f1c337214ff30424a594083
César Espirandelli <espirandelli@gmail.com>
2023-09-02 15:20:07 -0300

    Declare variables outside return

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -10,11 +10,10 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App(){
-    let message = 'Bye bye!';
-    if (Math.random() > 0.5 ){
-        message = 'Hello there!';
-    }
-    return <h1>{message}</h1>;
+    const date = new Date();
+    const time = date.toLocaleTimeString();
+
+    return <h1>{time}</h1>;
 }
 
 // 5) Show the content on the screen
