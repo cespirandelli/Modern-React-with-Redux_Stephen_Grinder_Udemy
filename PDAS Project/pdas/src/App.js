8dccf7f71a14f75ea72cee78d32062e407f7bdb8
César Espirandelli <espirandelli@gmail.com>
2023-09-03 19:47:04 -0300

    Using props to flow image from parent to child

--- PDAS Project/pdas/src/App.js
+++ PDAS Project/pdas/src/App.js
@@ -3,19 +3,23 @@ import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
 import SiriImage from "./images/siri.png";
 
-console.log(AlexaImage); // Base 64 will transform the image of 9,7Kb or less to a string
-console.log(SiriImage); // Will be shows as a path to the image if bigger then 9,7Kb.
-
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <img src={AlexaImage} />
-      <img src={SiriImage} />
-
-      <ProfileCard img="AlexaImage" title="Alexa" handle="@alexa99" />
-      <ProfileCard CortanaImage title="Cortana" handle="@cortana32" />
-      <ProfileCard SiriImage title="Siri" handle="@siri01" />
+      <ProfileCard
+        img="AlexaImage"
+        title="Alexa"
+        handle="@alexa99"
+        image={AlexaImage}
+      />
+      <ProfileCard
+        CortanaImage
+        title="Cortana"
+        handle="@cortana32"
+        image={CortanaImage}
+      />
+      <ProfileCard SiriImage title="Siri" handle="@siri01" image={SiriImage} />
     </div>
   );
 }
--- PDAS Project/pdas/src/ProfileCard.js
+++ PDAS Project/pdas/src/ProfileCard.js
@@ -1,7 +1,7 @@
-function ProfileCard({ title, handle }) {
-  console.log(title, handle);
+function ProfileCard({ title, handle, image }) {
   return (
     <div>
+      <img src={image} />
       <div>Title is {title} </div>
       <div>Handle is {handle} </div>
     </div>
