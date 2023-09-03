cdeca482708c673bf687bf4435fa93cf693f3950
César Espirandelli <espirandelli@gmail.com>
2023-09-03 02:46:50 -0300

    Update index.js

--- PDAS Project/pdas/src/index.js
+++ PDAS Project/pdas/src/index.js
@@ -1,3 +1,13 @@
+// Import Libs
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+// Get a reference to the div with ID Root on document and create root
+const el = document.getElementById("root");
+
+// Tell react to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// Show content on the screen
+root.render(<App />);
