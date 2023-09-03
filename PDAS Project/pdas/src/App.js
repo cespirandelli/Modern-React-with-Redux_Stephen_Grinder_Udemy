15dfc4cb64e499a11597baad1f2d08d71e7a3f12
César Espirandelli <espirandelli@gmail.com>
2023-09-03 19:29:58 -0300

    Update App.js
    Image import directly through folder. Different console.log for each image, depending on its size.

--- PDAS Project/pdas/src/App.js
+++ PDAS Project/pdas/src/App.js
@@ -1,13 +1,21 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+console.log(AlexaImage); // Base 64 will transform the image of 9,7Kb or less to a string
+console.log(SiriImage); // Will be shows as a path to the image if bigger then 9,7Kb.
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
+      <img src={AlexaImage} />
+      <img src={SiriImage} />
 
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
+      <ProfileCard img="AlexaImage" title="Alexa" handle="@alexa99" />
+      <ProfileCard CortanaImage title="Cortana" handle="@cortana32" />
+      <ProfileCard SiriImage title="Siri" handle="@siri01" />
     </div>
   );
 }
