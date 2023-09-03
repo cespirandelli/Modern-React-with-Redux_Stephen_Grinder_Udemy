38a5442e0d777ef021fac490c945960a066e463a
César Espirandelli <espirandelli@gmail.com>
2023-09-03 04:14:43 -0300

    Update ProfileCard.js
    Deconstructing feature from JS. One line for variable creation and association with props.

--- PDAS Project/pdas/src/ProfileCard.js
+++ PDAS Project/pdas/src/ProfileCard.js
@@ -1,6 +1,10 @@
 function ProfileCard(props) {
-  const title = props.title;
-  const handle = props.handle;
+  //   const title = props.title;
+  //   const handle = props.handle;
+
+  // The line below, does the same as both commented above. This is a JS destructuring feature
+  const { title, handle } = props;
+  // You create two variables that receives props.{object_value}
 
   return (
     <div>
