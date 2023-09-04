0953b7423f1a83fc974c8a9aa249454c61d96c48
César Espirandelli <espirandelli@gmail.com>
2023-09-04 19:17:48 -0300

    Update App.js
    Deconstructing even more

--- Animals Project/animals/src/App.js
+++ Animals Project/animals/src/App.js
@@ -5,10 +5,12 @@ function App() {
   function makeArray() {
     return [1, 10, 30, 50];
   }
-  const myArray = makeArray();
-  const firstElement = myArray[0];
-  const secondElement = myArray[1];
-  // This code below will print 1 and 10 on console
+  //   const myArray = makeArray();
+  //   const firstElement = myArray[0];
+  //   const secondElement = myArray[1];
+
+  // This code below also will print 1 and 10 on console same way as before
+  const [firstElement, secondElement] = makeArray();
   console.log(firstElement, secondElement);
 
   //   const [count, setCount] = useState(0);
