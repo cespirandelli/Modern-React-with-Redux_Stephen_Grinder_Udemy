b3fde65fd339fb88bbcb447150608beeaa62c6ff
César Espirandelli <espirandelli@gmail.com>
2023-09-04 20:51:58 -0300

    Update App.js
    Create function to get random animal from array with math.floor and math.random.  Console.log for testing.

--- Animals Project/animals/src/App.js
+++ Animals Project/animals/src/App.js
@@ -1,26 +1,22 @@
 import { useState } from "react";
 
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "dog", "gator", "cow", "horse"];
+  // If we don't add the Math.floor, this will generate a decimal number.
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+console.log(getRandomAnimal());
+
 function App() {
-  // Demonstration of what deconstructing is:
-  function makeArray() {
-    return [1, 10, 30, 50];
-  }
-  //   const myArray = makeArray();
-  //   const firstElement = myArray[0];
-  //   const secondElement = myArray[1];
+  const [count, setCount] = useState(0);
 
-  // This code below also will print 1 and 10 on console same way as before
-  const [firstElement, secondElement] = makeArray();
-  console.log(firstElement, secondElement);
-
-  //   const [count, setCount] = useState(0);
-
-  //   return (
-  //     <div>
-  //       <button onClick={() => setCount(count + 1)}>Add animal!</button>
-  //       <div> Number of animals: {count}</div>
-  //     </div>
-  //   );
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Add animal!</button>
+      <div> Number of animals: {count}</div>
+    </div>
+  );
 }
 
 export default App;
