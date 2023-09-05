c49cd97d6efbc8d53979a61af6a778125199f498
César Espirandelli <espirandelli@gmail.com>
2023-09-04 21:10:48 -0300

    Update App.js
    On the new comment section i tell about why we don't wan't to write the code with .push. This would modify the piece of state directly.

--- Animals Project/animals/src/App.js
+++ Animals Project/animals/src/App.js
@@ -14,6 +14,11 @@ function App() {
     // existing elements of animals array nad put them into a new one
     // Then we'll gona put a random animal at the end as well.
     setAnimals([...animals, getRandomAnimal()]);
+
+    // It's possible to do "the same as above", as the code below:
+    // animals.push( getRandomAnimal() )
+    // But this would modify directly the 'piece of state'
+    // so don't do like this.
   };
 
   return (
