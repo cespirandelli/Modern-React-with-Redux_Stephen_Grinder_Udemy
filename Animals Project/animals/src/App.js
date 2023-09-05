45b3021cb5ce8e38a571ade3cb1c8a1db1aa99d9
César Espirandelli <espirandelli@gmail.com>
2023-09-04 21:12:56 -0300

    Update App.js
    Print on screen what we are getting from animals array.

--- Animals Project/animals/src/App.js
+++ Animals Project/animals/src/App.js
@@ -10,20 +10,13 @@ function App() {
   const [animals, setAnimals] = useState([]);
 
   const handleClick = () => {
-    // The code below will get the array with all the
-    // existing elements of animals array nad put them into a new one
-    // Then we'll gona put a random animal at the end as well.
     setAnimals([...animals, getRandomAnimal()]);
-
-    // It's possible to do "the same as above", as the code below:
-    // animals.push( getRandomAnimal() )
-    // But this would modify directly the 'piece of state'
-    // so don't do like this.
   };
 
   return (
     <div>
       <button onClick={handleClick}>Add animal!</button>
+      <div>{animals}</div>
     </div>
   );
 }
