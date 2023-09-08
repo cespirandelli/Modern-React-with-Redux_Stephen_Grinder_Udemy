67ca2c45be565842f5c5139113e069b7c41761d1
César Espirandelli <espirandelli@gmail.com>
2023-09-07 22:29:01 -0300

    Update SearchBar.js

--- Pics project/pics/src/components/SearchBar.js
+++ Pics project/pics/src/components/SearchBar.js
@@ -1,13 +1,24 @@
-function SearchBar() {
+import { useState } from "react";
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("cars");
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    console.log("I need to tell the parent about some data!");
+    onSubmit("asdf");
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value.replace(/[a-z]/, ""));
+    // the code above, determines that the input cannot be lowercase letter.
   };
 
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
-        <input name="search bar" />
+        Confirm the search: {term}
+        <input value={term} onChange={handleChange} name="search bar" />
+        {term.length < 3 && " Term must be longer then 3!"}
       </form>
     </div>
   );
