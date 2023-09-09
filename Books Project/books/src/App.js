560730c1d3a0052f9d21f362679bf420fcde98af
César Espirandelli <espirandelli@gmail.com>
2023-09-09 04:59:32 -0300

    App and components update

--- Books Project/books/src/App.js
+++ Books Project/books/src/App.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-  return <div>App</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    const updatedBooks = [...books, { id: 123, title }];
+    setBooks(updatedBooks);
+  };
+
+  return (
+    <div>
+      {books.length}
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- Books Project/books/src/components/BookCreate.js
+++ Books Project/books/src/components/BookCreate.js
@@ -1,5 +1,33 @@
-function BookCreate() {
-  return <div>BookCreate</div>;
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle("");
+  };
+
+  return (
+    <div className="book-create">
+      <h3>Add a book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Enter book title</label>
+        <input
+          className="input"
+          value={title}
+          onChange={handleChange}
+          name="bookTitle"
+        />
+        <button className="button">Create!</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
