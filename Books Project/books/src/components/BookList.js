3358d5b78238488023cae4003e83c6ff3eb311f8
César Espirandelli <espirandelli@gmail.com>
2023-10-04 01:08:16 -0300

    Refactored code to use Context Provider

--- Books Project/books/src/components/BookList.js
+++ Books Project/books/src/components/BookList.js
@@ -1,23 +1,15 @@
-import { useContext } from 'react';
-import BooksContext from '../context/books';
-import BookShow from './BookShow';
+import { useContext } from "react";
+import BooksContext from "../context/books";
+import BookShow from "./BookShow";
 
-function BookList({ books, onDelete, onEdit }) {
-  const { count, incrementCount } = useContext(BooksContext);
-
+function BookList() {
+  const { books } = useContext(BooksContext);
+  // It's much easier to just pass the book object to the BookShow component and let it handle the rendering of the book. This way, we don't have to access context to handle each BookShow component.
   const renderedBooks = books.map((book) => {
-    return (
-      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
-    );
+    return <BookShow key={book.id} book={book} />;
   });
 
-  return (
-    <div className="book-list">
-      {count}
-      <button onClick={incrementCount}>Click</button>
-      {renderedBooks}
-    </div>
-  );
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
