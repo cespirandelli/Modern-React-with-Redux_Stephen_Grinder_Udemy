2b53959f89862fb5c0493e6439390d0501c46e6c
cespirandelli <espirandelli@gmail.com>
2023-10-19 17:06:14 -0300

    Added react-icon and tailwind-merge libs to project

--- Components Project/comps/src/App.js
+++ Components Project/comps/src/App.js
@@ -1,28 +1,46 @@
+import {
+  GoBell,
+  GoCopilot,
+  GoCreditCard,
+  GoDatabase,
+  GoEye,
+} from "react-icons/go";
 import Button from "./Button";
 
 function App() {
   return (
     <div>
       <div>
-        <Button success rounded outline>
+        <Button secondary outline rounded>
+          <GoBell />
           Click me!!
         </Button>
       </div>
+
       <div>
         <Button danger outline>
+          <GoCopilot />
           Buy now
         </Button>
       </div>
+
       <div>
-        <Button warning>See deal</Button>
+        <Button warning>
+          <GoCreditCard />
+          See deal
+        </Button>
       </div>
+
       <div>
         <Button secondary outline>
+          <GoDatabase />
           Hide Ads
         </Button>
       </div>
+
       <div>
         <Button primary rounded>
+          <GoEye />
           Something
         </Button>
       </div>
